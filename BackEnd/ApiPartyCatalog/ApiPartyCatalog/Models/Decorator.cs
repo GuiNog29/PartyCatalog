e9e80e9e737e4dd39c3baf123fcf7853c30eea11
Guilherme Almeida <guimathiasgui@gmail.com>
2023-03-23 19:15:45 -0300

    Adicionando models

---
+++ BackEnd/ApiPartyCatalog/ApiPartyCatalog/Models/Decorator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiPartyCatalog.Models
+{
+    public class Decorator
+    {
+        public Decorator()
+        {
+            Decorations = new Collection<Decoration>();
+        }
+
+        public int DecoratorId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string NameDecorator { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Password { get; set; }
+        public ICollection<Decoration> Decorations { get; set; }
+    }
+}
