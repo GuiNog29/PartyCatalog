50970d24d1e756ef9c602e487269cb96ccac5dfd
Guilherme Almeida <guimathiasgui@gmail.com>
2023-03-23 19:20:42 -0300

    Adicionando arquivo de contexto - 23/03

---
+++ BackEnd/ApiPartyCatalog/ApiPartyCatalog/Context/ApiPartyCatalogContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiPartyCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPartyCatalog.Context
+{
+    public class ApiPartyCatalogContext : DbContext
+    {
+        public ApiPartyCatalogContext(DbContextOptions<ApiPartyCatalogContext> options) : base(options)
+        { }
+
+        public DbSet<Decorator> Decorators { get; set; }
+        public DbSet<Decoration> Decorations { get; set; }
+    }
+}
