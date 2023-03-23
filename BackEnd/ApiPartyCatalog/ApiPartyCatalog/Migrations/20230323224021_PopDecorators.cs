156f1783a9dd666f3297590658f1245945f8aaad
Guilherme Almeida <guimathiasgui@gmail.com>
2023-03-23 20:34:55 -0300

    Populando tabelas no banco

---
+++ BackEnd/ApiPartyCatalog/ApiPartyCatalog/Migrations/20230323224021_PopDecorators.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiPartyCatalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopDecorators : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Decorators(NameDecorator, Email, Password) Values('Guilherme', 'guilherme@gmail.com', '123')");
+            mb.Sql("Insert into Decorators(NameDecorator, Email, Password) Values('Carlos', 'carlos@gmail.com', '123')");
+            mb.Sql("Insert into Decorators(NameDecorator, Email, Password) Values('Julia', 'julia@gmail.com', '123')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Decorators");
+        }
+    }
+}
