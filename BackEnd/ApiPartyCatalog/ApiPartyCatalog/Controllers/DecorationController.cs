c9c8077965c1cad6f9cfc44ee59411a5d41229e0
Guilherme Almeida <guimathiasgui@gmail.com>
2023-04-03 22:04:11 -0300

    Deletando decoracao e listando todas decoracoes

--- BackEnd/ApiPartyCatalog/ApiPartyCatalog/Controllers/DecorationController.cs
+++ BackEnd/ApiPartyCatalog/ApiPartyCatalog/Controllers/DecorationController.cs
@@ -16,6 +16,17 @@ namespace ApiPartyCatalog.Controllers
             _context = context;
         }
 
+        [Route("GetAllDecorarions"), HttpGet]
+        public ActionResult<IEnumerable<Decoration>> GetAllDecorarions()
+        {
+            var decorations = _context.Decorations.ToList();
+
+            if (decorations.Count == 0 || decorations is null)
+                return NotFound("Não foi encontrado nenhuma decoracao...");
+
+            return Ok(decorations);
+        }
+
         [Route("GetAllDecorationFromDecorator"), HttpGet]
         public ActionResult<IEnumerable<Decoration>> GetAllDecorationFromDecorator(int idDecorator)
         {
@@ -59,6 +70,20 @@ namespace ApiPartyCatalog.Controllers
 
             _context.Entry(decoration).State = EntityState.Modified;
             _context.SaveChanges(); 
+
+            return Ok(decoration);
+        }
+
+        [Route("DeleteDecoration"), HttpDelete]
+        public ActionResult DeleteDecoration(int idDecoration)
+        {
+            var decoration = _context.Decorations.FirstOrDefault(x => x.DecorationId == idDecoration);
+
+            if (decoration is null)
+                return BadRequest("Decoração não localizada...");
+
+            _context.Decorations.Remove(decoration);
+            _context.SaveChanges();
 
             return Ok(decoration);
         }
