9879bfe499436207de8c7a2a4199bca744375761
Guilherme Almeida <guimathiasgui@gmail.com>
2023-04-04 22:08:42 -0300

    Listando decorador a partir do nome - 04/04

--- BackEnd/ApiPartyCatalog/ApiPartyCatalog/Controllers/DecoratorController.cs
+++ BackEnd/ApiPartyCatalog/ApiPartyCatalog/Controllers/DecoratorController.cs
@@ -15,15 +15,26 @@ namespace ApiPartyCatalog.Controllers
             _context = context;
         }
 
-        [HttpGet]
+        [Route("GetDecorators"), HttpGet]
         public ActionResult<IEnumerable<Decorator>> GetDecorators() 
         {
             var decorators = _context.Decorators.ToList();
 
-            if(decorators is null)
+            if(decorators.Count == 0 || decorators is null)
                 return NotFound("Não foi encontrado nenhum decorador");
 
-            return decorators;
+            return Ok(decorators);
+        }
+
+        [Route("GetDecoratorByName"), HttpGet]
+        public ActionResult<IEnumerable<Decorator>> GetDecoratorByName(string name)
+        {
+            var decorator = _context.Decorators.Where(x => x.NameDecorator.Contains(name)).ToList();
+
+            if (decorator.Count == 0 || decorator == null)
+                return NotFound("Não foi encontrado um decorador com o nome informado");
+
+            return Ok(decorator);
         }
     }
 }
